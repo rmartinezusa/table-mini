b5fe41ff8b0eb7501f4122656209de9f242cb918
Rogelio Martinez <rogejr89@gmail.com>
2024-10-14 15:25:46 -0400

    GP

--- api/restaurants.js
+++ api/restaurants.js
@@ -5,3 +5,45 @@ module.exports = router;
 const prisma = require("../prisma");
 
 // TODO: routes!
+
+
+router.get("/", async (req, res, next) => {
+    try {
+      const restaurants = await prisma.restaurant.findMany(); 
+      res.json(restaurants);
+    } catch (e) {
+      next(e);
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        // this returns a the restaurant with the nested reservations
+        const restaurant = await prisma.restaurant.findUniqueOrThrow({
+            where: { id: +id },
+            include: { reservations: true },
+        });
+        res.json(restaurant);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.post("/:id/reservations", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name, email, partySize } = req.body;
+        const reservation = await prisma.reservation.create({
+            data: {
+                name,
+                email,
+                partySize: +partySize,
+                restaurantId: +id 
+            },
+        });
+        res.status(201).json(reservation);
+    } catch (e) {
+        next(e);
+    }
+});
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,11 +1,68 @@
 const prisma = require("../prisma");
-const seed = async () => {
-  // TODO: Create 3 restaurants with 5 reservations each
+
+const { faker } = require("@faker-js/faker");
+const seed = async (numRestaurants = 3, numReservations = 5) => {
+  // A loop must be used because `prisma.restaurant.createMany` fails here
+  for (let i = 0; i < numRestaurants; i++) {
+    // For each restaurant, create an array of reservations
+    const reservations = Array.from({ length: numReservations }, (_, j) => {
+      const name = faker.internet.displayName();
+      return {
+        name,
+        email: `${name}@foo.bar`,
+        partySize: faker.number.int({ min: 1, max: 10 }),
+      };
+    });
+
+    // Create a single restaurant with nested reservations
+    await prisma.restaurant.create({
+      data: {
+        name: faker.company.buzzAdjective() + " " + faker.company.buzzNoun(),
+        reservations: {
+          create: reservations,
+        },
+      },
+    });
+  }
 };
+
 seed()
-  .then(async () => await prisma.$disconnect())
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+.then(async () => await prisma.$disconnect())
+.catch(async (e) => {
+  console.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
+});
+
+/*
+const seed = async () => {
+
+  for (let i = 0; i < 3; i++) {
+    const reservations = [];
+    for (let j = 0; j < 5; j++) {
+      reservations.push({
+        name: `Person ${i}${j}`,
+        email: `${i}${j}@foo.bar`,
+        partySize: Math.floor(Math.random() * 10) + 1,
+      });
+    }
+
+    await prisma.restaurant.create({
+      data: {
+        name: `Restaurant ${i + 1}`,
+        reservations: {
+          create: reservations,
+        },
+      },
+    });
+  }
+};
+
+seed()
+.then(async () => await prisma.$disconnect())
+.catch(async (e) => {
+  console.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
+});
+*/
